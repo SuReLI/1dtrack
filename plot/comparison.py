93c84f646c8e42ceafe5b7872f63f0332db59d9c
erwanlecarpentier <erwan.lc2@gmail.com>
2017-10-12 18:28:57 +0200

    Number of calls added to plot method

--- plot/comparison.py
+++ plot/comparison.py
@@ -16,7 +16,7 @@ BLUE = '#333399';
 ORNG = '#ff6600';
 GREE = '#00cc66';
 
-take_log = True # Take the log of the data or not
+take_log = False # Take the log of the data or not
 repo = "data/backup/short_track/" # Path to the saved data
 fp_range = [ # Different failure probabilities (wrt the files names)
     "00", "01", "02", "03",
@@ -41,15 +41,19 @@ for fp in fp_range:
     d1 = pd.read_csv(path1,sep = ',')
     loss1 = d1["score"]
     cput1 = d1["computational_cost"]
+	calls1 = d1["nb_calls"]
     d2 = pd.read_csv(path2,sep = ',')
     loss2 = d2["score"]
     cput2 = d2["computational_cost"]
+	calls2 = d2["nb_calls"]
 
     if take_log:
         loss1 = np.log(loss1)
         loss2 = np.log(loss2)
         cput1 = np.log(cput1)
         cput2 = np.log(cput2)
+		calls2 = np.log(calls2)
+		calls1 = np.log(calls1)
 
     # Store the data
     loss1_means.append(loss1.mean())
@@ -62,17 +66,30 @@ for fp in fp_range:
     cput2_means.append(cput2.mean())
     cput2_stdev.append(cput2.std())
 
-f, (ax1, ax2) = plt.subplots(2, 1, sharey=True)
+    calls1_means.append(calls1.mean())
+    calls1_stdev.append(calls1.std())
+    calls2_means.append(calls2.mean())
+    calls2_stdev.append(calls2.std())
+
+f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharey=True)
+
 l1 = ax1.errorbar(pl_range, loss1_means, color=BLUE, yerr=loss1_stdev, fmt='o')
 l2 = ax1.errorbar(pl_range, loss2_means, color=ORNG, yerr=loss2_stdev, fmt='o')
 ax1.set_xlabel('Transition failure probability')
 ax1.set_ylabel('Loss')
-ax1.legend([l1, l2], ['Vanilla UCT', 'Experimental UCT'],numpoints=1,loc='lower right')
+ax1.legend([l1, l2], ['Vanilla UCT', 'OLUCT'],numpoints=1,loc='lower right')
 
 l1 = ax2.errorbar(pl_range, cput1_means, color=BLUE, yerr=cput1_stdev, fmt='o')
 l2 = ax2.errorbar(pl_range, cput2_means, color=ORNG, yerr=cput2_stdev, fmt='o')
 ax2.set_xlabel('Transition failure probability')
 ax2.set_ylabel('Computational cost')
-ax2.legend([l1, l2], ['Vanilla UCT', 'Experimental UCT'],numpoints=1,loc='lower right')
+ax2.legend([l1, l2], ['Vanilla UCT', 'OLUCT'],numpoints=1,loc='lower right')
+
+l1 = ax1.errorbar(pl_range, calls1_means, color=BLUE, yerr=calls1_stdev, fmt='o')
+l2 = ax1.errorbar(pl_range, calls2_means, color=ORNG, yerr=calls2_stdev, fmt='o')
+ax3.set_xlabel('Transition failure probability')
+ax3.set_ylabel('Number of calls')
+ax3.legend([l1, l2], ['Vanilla UCT', 'OLUCT'],numpoints=1,loc='lower right')
 
 plt.show()
+
