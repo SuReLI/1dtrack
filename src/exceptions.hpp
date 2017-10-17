218c4495bab6dc7b7fd9905c06494ade8c53c5d4
erwanlecarpentier <erwan.lc2@gmail.com>
2017-10-17 18:51:45 +0200

    Exceptions created for configuration file parsing.

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -4,14 +4,42 @@
 /**
  * @brief Decision criterion selector exception
  *
- * Exception for wron decision criterion selector choice.
+ * Exception for wrong decision criterion selector choice.
  */
 struct decision_criterion_selector_exception : std::exception {
     explicit decision_criterion_selector_exception() noexcept {}
     virtual ~decision_criterion_selector_exception() noexcept {}
 
     virtual const char * what() const noexcept override {
-        return "wrong decision criterion selector";
+        return "wrong decision criterion selector.\n";
+    }
+};
+
+/**
+ * @brief Action names configuration file exception
+ *
+ * Exception for wrong action names choice in configuration file.
+ */
+struct action_names_configuration_file_exception : std::exception {
+    explicit action_names_configuration_file_exception() noexcept {}
+    virtual ~action_names_configuration_file_exception() noexcept {}
+
+    virtual const char * what() const noexcept override {
+        return "in config file: actions names do not match.\n";
+    }
+};
+
+/**
+ * @brief Wrong syntax configuration file exception
+ *
+ * Exception for wrong syntax in configuration file.
+ */
+struct wrong_syntax_configuration_file_exception : std::exception {
+    explicit wrong_syntax_configuration_file_exception() noexcept {}
+    virtual ~wrong_syntax_configuration_file_exception() noexcept {}
+
+    virtual const char * what() const noexcept override {
+        return "in config file: please make sure that the syntaxes match.\n";
     }
 };
 
