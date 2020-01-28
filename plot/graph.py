f9257c6299433d37d9f5987bf9bece3672bd41dd
erwanlecarpentier <erwan.lc2@gmail.com>
2020-01-28 16:28:37 +0100

    Minor update

--- plot/graph.py
+++ plot/graph.py
@@ -7,11 +7,17 @@ Set the correct paths and run this script.
 edit: 26/09/2017
 """
 
+from matplotlib.ticker import FuncFormatter
 from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
+import matplotlib
+
+# Use Type 1 fonts
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
 BLUE = '#333399';
 ORANGE = '#ff6600';
@@ -34,7 +40,9 @@ PL3 = '#ff6600';
 PL4 = '#ee1a7a';
 PL5 = '#6699ff';
 
-def plot_error_bar(ax, rng, mean, stddev, cl, mrk, fcc, lw, ls):
+def plot_error_bar(ax, rng, mean, stddev, cl, mrk, fcc, lw, ls, take_log):
+	if take_log:
+		ax.set_yscale('log')
 	l = ax.errorbar(rng, mean, color=cl, yerr=stddev, fmt=mrk, linewidth=lw, linestyle=ls, markersize=8, markerfacecolor=fcc)
 	v = np.copy(stddev)
 	up = np.add(mean, +v)
@@ -43,11 +51,11 @@ def plot_error_bar(ax, rng, mean, stddev, cl, mrk, fcc, lw, ls):
 	return l
 
 def plot_relative_means(ax, rng, mean, stddev, cl, mrk, fcc, lw, ls, uct_lo_mea):
-	values = [a - b for a, b in zip(mean, uct_lo_mea)]
+	values = [(a - b)/b for a, b in zip(mean, uct_lo_mea)]
 	l = ax.plot(rng, values, color=cl, marker=mrk, linewidth=lw, linestyle=ls, markersize=8, markerfacecolor=fcc)
 	return l
 
-def extract(path, failure_probability_range, take_log):
+def extract(path, failure_probability_range):
 	lomns = []
 	lostd = []
 	cpmns = []
@@ -60,10 +68,6 @@ def extract(path, failure_probability_range, take_log):
 		lo = data["score"]
 		cp = data["computational_cost"]
 		ca = data["nb_calls"]
-		if take_log:
-			lo = np.log(lo)
-			cp = np.log(cp)
-			ca = np.log(ca)
 		lomns.append(lo.mean())
 		lostd.append(lo.std())
 		cpmns.append(cp.mean())
@@ -80,19 +84,18 @@ def plot(path, args, color, mrk, fcc, lw, ls, uct_lo_mea):
 	ax2             = args[4]
 	ax3             = args[5]
 	ax4             = args[6]
-	[lo_mea, lo_std, cp_mea, cp_std, ca_mea, ca_std] = extract(path,fp_range,take_log)
-	l = plot_error_bar(ax1, fp_range_val, lo_mea, lo_std, color, mrk, fcc, lw, ls)
+	[lo_mea, lo_std, cp_mea, cp_std, ca_mea, ca_std] = extract(path,fp_range)
+	l = plot_error_bar(ax1, fp_range_val, lo_mea, lo_std, color, mrk, fcc, lw, ls, take_log)
 	l = plot_relative_means(ax2, fp_range_val, lo_mea, lo_std, color, mrk, fcc, lw, ls, uct_lo_mea)
-	l = plot_error_bar(ax3, fp_range_val, cp_mea, cp_std, color, mrk, fcc, lw, ls)
-	l = plot_error_bar(ax4, fp_range_val, ca_mea, ca_std, color, mrk, fcc, lw, ls)
+	l = plot_error_bar(ax3, fp_range_val, cp_mea, cp_std, color, mrk, fcc, lw, ls, take_log)
+	l = plot_error_bar(ax4, fp_range_val, ca_mea, ca_std, color, mrk, fcc, lw, ls, take_log)
 	return l
 
-def apply_label(ax, xlab, ylab, ylab_log, font, take_log):
+def apply_label(ax, xlab, ylab, font, per):
 	ax.set_xlabel(xlab, fontproperties=font)
-	if take_log:
-		ax.set_ylabel(ylab_log, fontproperties=font)
-	else:
-		ax.set_ylabel(ylab, fontproperties=font)
+	ax.set_ylabel(ylab, fontproperties=font)
+	if per:
+		ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y))) 
 
 # Variables --------------------------------------------------------------------
 
@@ -110,13 +113,14 @@ fp_range_val = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 
 plt.close('all')
 
-f, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True, figsize=(9.4,12.6))
+f, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True, figsize=(8.0,15.0))
+ax1.set_xlim(0.0, 0.5)
 
 if discrete:
 	root_path = 'data/backup/discrete/short_plc'
 else:
 	root_path = 'data/backup/continuous/long_plc'
-[uct_lo_mea, uct_lo_std, uct_cp_mea, uct_cp_std, uct_ca_mea, uct_ca_std] = extract(root_path + '0',fp_range,take_log)
+[uct_lo_mea, uct_lo_std, uct_cp_mea, uct_cp_std, uct_ca_mea, uct_ca_std] = extract(root_path + '0',fp_range)
 lw = 2.3
 args = [fp_range, fp_range_val, take_log, ax1, ax2, ax3, ax4]
 l1 = plot(root_path + '1_b0', args, PL1, 's', PL1,    lw, '-', uct_lo_mea)
@@ -127,7 +131,10 @@ l4 = plot(root_path + '1_b3', args, PL4, 's', 'none', lw, '-.', uct_lo_mea)
 l5 = plot(root_path + '1_b4', args, PL5, '^', 'none', lw, '--', uct_lo_mea)
 l0 = plot(root_path + '0',    args, PL0, 'o', PL0,    lw, '-', uct_lo_mea)
 
-
+#ax1.set_ylim(5000, 0)
+ax1.set_ylim(top=10)
+ax3.set_ylim(top=10)
+ax4.set_ylim(top=10000)
 # Label ------------------------------------------------------------------------
 
 font = FontProperties()
@@ -139,29 +146,24 @@ font.set_weight('light')
 
 labsz = 15
 xlab = 'Transition misstep probability'
-ylab1 = 'Loss (time steps to the goal)'
-ylab1_log = 'Log of the loss\n(time steps to\nthe goal)'
-ylab2 = 'Mean loss\nrelatively to UCT'
-ylab2_log = 'Log of the mean loss\nrelatively to UCT'
-ylab3 = 'Computational cost (ms)'
-ylab3_log = 'Log of the\ncomputational\ncost (ms)'
+ylab1 = 'Loss (time steps\nto the goal)'
+ylab2 = 'Mean loss relatively\nto OLUCT loss'
+ylab3 = 'Computational\ncost (ms)'
 ylab4 = 'Number of calls'
-ylab4_log = 'Log of the\nnumber of calls'
 
-apply_label(ax1,xlab,ylab1,ylab1_log,font,take_log)
-apply_label(ax2,xlab,ylab2,ylab2_log,font,take_log)
-apply_label(ax3,xlab,ylab3,ylab3_log,font,take_log)
-apply_label(ax4,xlab,ylab4,ylab4_log,font,take_log)
+apply_label(ax1,xlab,ylab1,font,False)
+apply_label(ax2,xlab,ylab2,font,True)
+apply_label(ax3,xlab,ylab3,font,False)
+apply_label(ax4,xlab,ylab4,font,False)
 
 # Legend -----------------------------------------------------------------------
 if discrete:
 	l = [l0,l1,l2,l3,l4,l5]
-	lg = ['Vanilla UCT', 'Plain OLUCT', 'SDM-OLUCT', 'SDV-OLUCT', 'SDSD-OLUCT', 'RDV-OLUCT']
+	lg = ['OLUCT', 'Plain OLTA', 'SDM-OLTA', 'SDV-OLTA', 'SDSD-OLTA', 'RDV-OLTA']
 else:
 	l = [l0,l1,l3,l4,l5]
-	lg = ['Vanilla UCT', 'Plain OLUCT', 'SDV-OLUCT', 'SDSD-OLUCT', 'RDV-OLUCT']
+	lg = ['OLUCT', 'Plain OLTA', 'SDV-OLTA', 'SDSD-OLTA', 'RDV-OLTA']
 
-ax4.legend(l, lg, numpoints=1, prop=font, bbox_to_anchor=(0., -0.6, 1., .102), loc='lower center', ncol=3, mode="expand", borderaxespad=0., frameon=False)
+ax4.legend(l, lg, numpoints=1, prop=font, bbox_to_anchor=(0., -0.8, 1., .102), loc='lower center', ncol=3, mode="expand", borderaxespad=0., frameon=False)
 
 plt.show()
-
